485f88df56ae824b50ce18f412f9fa90ce5a952f
TIAGO COLADONATO <46878457@ort>
2023-09-14 09:27:56 -0300

    template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
+import GlobalStyles from './globalStyles'
+import { Navbar, Footer } from './components';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <>
-      
-      <BrowserRouter>
-
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-
-
-
-    </>
+    
+      <Router>
+          <GlobalStyles />
+          <ScrollToTop />
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/services' component={Services} />
+            <Route path='/products' component={Products} />
+            <Route path='/sign-up' component={SignUp} />
+          </Switch>
+          <Footer />
+      </Router>
+        
+    
   );
 }
 
